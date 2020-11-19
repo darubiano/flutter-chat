31b6197e35452f34b10cb91fa64544ff98b512b9
darubiano57 <darubiano57@ucatolica.edu.co>
2020-11-18 23:05:56 -0500

    ultimos cambios añadir icono

--- lib/global/Enviroments.dart
+++ lib/global/Enviroments.dart
@@ -2,13 +2,14 @@ import 'dart:io';
 
 // Vriable globales
 // no funciona con la ip http://127.0.0.1:3000/ toca con la ip del pc ipconfig
+// Para probar en local 'http://192.168.0.8:3000/api'
 class Enviroments {
   static String apiURL= Platform.isAndroid 
-                        ? 'http://192.168.0.8:3000/api' 
-                        : 'http://localhost:3000/api';
+                        ? 'https://flutter-chat-node.herokuapp.com/api' 
+                        : 'https://flutter-chat-node.herokuapp.com/api';
   static String socketURL = Platform.isAndroid 
-                        ? 'http://192.168.0.8:3000/' 
-                        : 'http://localhost:3000';
+                        ? 'https://flutter-chat-node.herokuapp.com/' 
+                        : 'https://flutter-chat-node.herokuapp.com/';
                   
 }
 
